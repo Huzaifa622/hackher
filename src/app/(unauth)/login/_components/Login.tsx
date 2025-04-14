633380e8332d92abcef4db0fddbb45b68712092e
huzaifa <thuzaifa07@gmail.com>
2025-04-14 20:51:01 +0500

    rhf form vlidation

--- src/app/(unauth)/login/_components/Login.tsx
+++ src/app/(unauth)/login/_components/Login.tsx
@@ -8,11 +8,12 @@ import {
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { z } from "zod";
-
+import { useForm } from "react-hook-form";
 import AdminLogin from "../actions";
 import toast from "react-hot-toast";
 import Loader from "@/components/ui/loader";
 import Logo from "./logo";
+import { zodResolver } from "@hookform/resolvers/zod";
 // import { AdminLogin } from "../actions";
 
 const LoginSchema = z.object({
@@ -24,49 +25,73 @@ type Login = z.infer<typeof LoginSchema>;
 
 export default function Login() {
   const [visible, setVisible] = useState(false);
-  const [data, setData] = useState<Login>({ email: "", password: "" });
-  const [error, setError] = useState<any>({});
-  const [loader , setLoader] =useState(false) // eslint-disable-line
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Login>({ resolver: zodResolver(LoginSchema) });
+  const [loader, setLoader] = useState(false); // eslint-disable-line
   const router = useRouter();
 
-  const handleLogin = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    const validation = LoginSchema.safeParse(data);
-    if (validation.success === false) {
-      return setError(validation?.error?.format());
-    }
-    setError({});
-    setLoader(true)
 
+  const formSubmit = async (data: Login) =>{
+    setLoader(true);
+    console.log(data)
     const res = await AdminLogin(data);
-    console.log(res)
-    if (res.status == true) {
-      toast.success(res.message);
-      setLoader(false)
-      localStorage.setItem("token", res.token);
-      localStorage.setItem("user", JSON.stringify(res.data));
-      localStorage.setItem("role" , res.data.role)
-      router.push("/hosts-guests-by-age");
-    }
+      
+      if (res.status == true) {
+        toast.success(res.message);
+        setLoader(false);
+        localStorage.setItem("token", res.token);
+        localStorage.setItem("user", JSON.stringify(res.data));
+        localStorage.setItem("role", res.data.role);
+        router.push("/hosts-guests-by-age");
+      }
+  
+      if (res.status == false) {
+        setLoader(false);
+        toast.error(res.message);
+      }
+  }
+  // const handleLogin = async (e: React.SyntheticEvent) => {
+  //   e.preventDefault();
+  //   const validation = LoginSchema.safeParse(data);
+  //   if (validation.success === false) {
+  //     return setError(validation?.error?.format());
+  //   }
+  //   setError({});
+  //   setLoader(true);
 
-    if (res.status == false) {
-      setLoader(false)
-      toast.error(res.message);
-    }
-  };
-  if(loader){
+  //   const res = await AdminLogin(data);
+  //   console.log(res);
+  //   if (res.status == true) {
+  //     toast.success(res.message);
+  //     setLoader(false);
+  //     localStorage.setItem("token", res.token);
+  //     localStorage.setItem("user", JSON.stringify(res.data));
+  //     localStorage.setItem("role", res.data.role);
+  //     router.push("/hosts-guests-by-age");
+  //   }
+
+  //   if (res.status == false) {
+  //     setLoader(false);
+  //     toast.error(res.message);
+  //   }
+  // };
+  if (loader) {
     return (
       <div className="flex justify-center w-screen h-screen items-center">
-        <Loader/>
+        <Loader />
       </div>
-    )
+    );
   }
   return (
     <div className="w-[90%] md:w-[30%] ">
       <div className="flex justify-center items-center py-12">
-        <Logo/>
+        <Logo />
       </div>
-      <div className=" rounded-sm p-4 py-12 shadow-lg bg-white ">
+      <form onSubmit={handleSubmit(formSubmit)} className=" rounded-sm p-4 py-12 shadow-lg bg-white ">
         <h1 className="text-2xl text-center text-[#262932] font-bold">
           Welcome{" "}
         </h1>
@@ -77,17 +102,14 @@ export default function Login() {
             <input
               type="email"
               placeholder="Email.."
-              value={data.email}
-              onChange={(e) =>
-                setData((prev) => ({ ...prev, email: e.target.value }))
-              }
+              {...register("email")}
               className="w-full text-sm p-2 pl-8 rounded-md border outline-none"
             />
             <EnvelopeClosedIcon className="absolute top-[30%] left-2" />
           </div>
-          {error.email && (
+          {errors.email && (
             <span className="text-red-500 text-xs">
-              {error.email._errors[0]}
+              {errors.email.message}
             </span>
           )}
         </div>
@@ -102,29 +124,24 @@ export default function Login() {
             </div>
             <input
               type={visible ? "text" : "password"}
-              value={data.password}
-              onChange={(e) =>
-                setData((prev) => ({ ...prev, password: e.target.value }))
-              }
+              {...register("password")}
               placeholder="Password.."
               className="w-full p-2 text-sm rounded-md border outline-none"
             />
           </div>
-          {error.password && (
-            <span className="text-red-500 text-xs">
-              {error.password._errors[0]}
-            </span>
+          {errors.password && (
+            <span className="text-red-500 text-xs">{errors.password.message}</span>
           )}
         </div>
-        <div
-          onClick={handleLogin}
-          className="text-center cursor-pointer py-1 flex bg-[#262932] rounded-sm justify-center"
+        <button
+          type="submit"
+          className="text-center w-full cursor-pointer py-1 flex bg-[#262932] rounded-sm justify-center"
         >
-          <button className=" flex justify-center items-center gap-2 text-sm rounded-sm p-1 text-white">
+          <div className=" flex justify-center items-center gap-2 text-sm rounded-sm p-1 text-white">
             Sign In <EnterIcon />
-          </button>
-        </div>
-      </div>
+          </div>
+        </button>
+      </form>
     </div>
   );
 }
